8bb3329ead455828a58e106df06b044fab877e8c
Alan Michel <130312363+AlanMichel30@users.noreply.github.com>
2024-05-10 15:59:39 -0300

    Formulário de Cadastro de Usuário

---
+++ Formulário de Cadastro de Usuário/cadastro_usuario.php
@@ -0,0 +1,15 @@
+<?php
+$nome = $_POST["fidNome"];
+$endereco = $_POST["fidEndereco"];
+$numero = $_POST["fidNumero"];
+$bairro = $_POST["fidBairro"];
+$cidade = $_POST["fidCidade"];
+$estado = $_POST["fidEstado"];
+$cep = $_POST["fidCEP"];
+$telefone = $_POST["fidTelefone"];
+$cpf = $_POST["fidCPF"];
+$estado_civil = $_POST["fidEstadoCivil"];
+$email = $_POST["fidEmail"];
+$senha = $_POST["fldSenha"];
+?>
+
